70991f645c2531ef88d3fed1a5a2fca69cf50c40
zinnli <limgx5192@gmail.com>
2026-02-08 18:20:12 +0900

    refactor: tailwind 마이그레이션 및 불요 라이브러리 삭제

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const currentPath = pathname.split("/")[1];
 
   const textBoldStyle = (path: string): string =>
-    `text-18 ${currentPath === path ? "text-black border-b border-b-primary_sub_30 font-bold" : "text-black/80"}  hover:text-primary`;
+    `text-18 ${currentPath === path ? "text-black border-b border-b-primary_sub/30 font-bold" : "text-black/80"}  hover:text-primary`;
 
   return (
     <header className="sticky top-0 flex justify-end w-[100%] max-w-[800px] gap-5 py-3 bg-gray_bg z-50">
